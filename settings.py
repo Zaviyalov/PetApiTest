3faee0b8fb57a00558fd9b52855f837b2644f959
denis <92470430+Mousport@users.noreply.github.com>
2023-09-11 14:10:57 +0300

    new_test_in_project add_new_test update_test_step_3 update_false_test

--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 valid_email = "denis321@mail.ru"
 valid_password = "12345"
+faled_user = "den32qa@mail.ru"
+faled_password = "32321"
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,7 +1,7 @@
 import os.path
 
 from api import PetFriends
-from settings import valid_password, valid_email
+from settings import valid_password, valid_email, faled_password, faled_user
 
 pf = PetFriends()
 
@@ -34,7 +34,7 @@ def test_successful_delete_self_pet():
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
 
     if len(my_pets['pets']) == 0:
-        pf.add_new_pet(auth_key, "Super-hero", "human", "20", "images/image_123.jpg")
+        pf.add_new_pets(auth_key, "Super-hero", "human", "20", "images/image_123.jpg")
         _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
 
     pet_id = my_pets['pets'][0]['id']
@@ -75,3 +75,69 @@ def test_add_photo_of_pets(pet_photo="image/images_123.jpg"):
     status, _ = pf.add_new_photo_of_pet(auth_key, pet_id, pet_photo)
 
     assert status == 200
+
+
+def test_get_api_key_not_correct_user(email=faled_user, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 403
+    assert 'key' not in result
+
+
+def test_get_api_key_not_correct_password(email=valid_email, password=faled_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 403
+    assert 'key' not in result
+
+
+def test_get_all_pets_not_correct_filter(filter="2222"):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 500
+
+
+def test_add_new_pet_with_not_correct_age(name="pinguen", animal_type="vampir",
+                                          age=1, pet_photo="image/horror023.png"):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    try:
+        status, result = pf.add_new_pets(auth_key, name, animal_type, age, pet_photo)
+    except:
+        print(" Ошибка ввода данных")
+
+def test_add_new_pet_with_not_correct_name(name={"pinguen": "momo"}, animal_type="vampir",
+                                          age=1, pet_photo="image/horror023.png"):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    try:
+        status, result = pf.add_new_pets(auth_key, name, animal_type, age, pet_photo)
+    except:
+        print(" Ошибка ввода данных")
+
+def test_add_new_pet_with_not_correct_animal_type(name="pinguen", animal_type=["vampir"],
+                                          age=1, pet_photo="image/horror023.png"):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    try:
+        status, result = pf.add_new_pets(auth_key, name, animal_type, age, pet_photo)
+    except:
+        print(" Ошибка ввода данных")
+
+def test_add_new_pet_with_not_correct_pet_photo(name="pinguen", animal_type="vampir",
+                                                age="1", pet_photo="orror023.png"):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    try:
+        status, result = pf.add_new_pets(auth_key, name, animal_type, age, pet_photo)
+    except:
+        print(" Ошибка ввода данных")
+
+
+def test_add_photo_of_pets_not_correct_id(pet_photo="image/images_123.jpg"):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = "ewqsd123"
+    status, _ = pf.add_new_photo_of_pet(auth_key, pet_id, pet_photo)
+
+    assert status == 500
+
